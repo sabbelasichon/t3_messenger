66fdba2f680d40110191c2c1ad0b4d2e4889bd53
Sebastian Schreiber <me@schreibersebastian.de>
2023-02-15 22:25:42 +0100

    TASK: Add environment specific configurations

---
+++ Tests/Functional/Fixtures/Extensions/t3_messenger_test/Configuration/test/Messenger.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "t3_messenger" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+use Ssch\T3Messenger\Tests\Functional\Fixtures\Extensions\t3_messenger_test\Classes\Command\MyCommand;
+use Ssch\T3Messenger\Tests\Functional\Fixtures\Extensions\t3_messenger_test\Classes\Command\MyFailingCommand;
+
+return [
+    'routing' => [
+        MyCommand::class => [
+            'senders' => ['async'],
+        ],
+        MyFailingCommand::class => [
+            'senders' => ['async'],
+        ],
+    ],
+];
